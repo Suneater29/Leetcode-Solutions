21d7415b9a37edc54f468c567629eff5656d6a9d
Arsalan Ansari <arsalan8974@gmail.com>
2026-07-24 22:28:28 +0530

    Time: 0 ms (100%), Space: 10.9 MB (93.55%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -19,7 +19,21 @@ public:
     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int>postorder;
-        makepost(root,postorder);
+        if(root==nullptr) return postorder;
+        stack<TreeNode*>st1;
+        stack<TreeNode*>st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=nullptr) st1.push(root->left);
+            if(root->right!=nullptr) st1.push(root->right);
+        }
+        while(!st2.empty()){
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
         return postorder;
     }
 };
