47063bb82b7774d2873986d72acc804489969789
Arsalan Ansari <arsalan8974@gmail.com>
2026-08-01 21:48:37 +0530

    Time: 0 ms (100%), Space: 22.2 MB (31.85%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,26 +12,32 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+        // if(root==nullptr){
+        //     return 0;
+        // }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // int height=0;
+        // while(!q.empty()){
+        //     int qsize=q.size();
+        //     for(int i=0;i<qsize;i++){
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         if(node->left!=nullptr){
+        //             q.push(node->left);
+        //         }
+        //         if(node->right!=nullptr){
+        //             q.push(node->right);
+        //         }
+        //     }
+        //     height++;
+        // }
+        // return height;
         if(root==nullptr){
             return 0;
         }
-        queue<TreeNode*>q;
-        q.push(root);
-        int height=0;
-        while(!q.empty()){
-            int qsize=q.size();
-            for(int i=0;i<qsize;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node->left!=nullptr){
-                    q.push(node->left);
-                }
-                if(node->right!=nullptr){
-                    q.push(node->right);
-                }
-            }
-            height++;
-        }
-        return height;
+        int leftDepth=maxDepth(root->left);
+        int rightDepth=maxDepth(root->right);
+        return 1+max(leftDepth,rightDepth);
     }
 };
