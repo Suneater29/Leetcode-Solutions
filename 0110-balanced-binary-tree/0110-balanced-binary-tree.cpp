87091bfd827ad10284a79c71e4092ed8fdc00a08
Arsalan Ansari <arsalan8974@gmail.com>
2026-08-03 18:25:39 +0530

    Time: 0 ms (100%), Space: 23.2 MB (21.05%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,8 +14,9 @@ public:
     int findHeight(TreeNode* root){
         if(root==nullptr) return 0;
         int leftHeight=findHeight(root->left);
+        if(leftHeight==-1) return -1;
         int rightHeight=findHeight(root->right);
-        if(leftHeight==-1 || rightHeight==-1) return -1;
+        if(rightHeight==-1) return -1;
         if(abs(leftHeight-rightHeight)>1) return -1;
         return 1+max(leftHeight,rightHeight);
     }
