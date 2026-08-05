3b0c92ca3796f09a99d2709ec553bce23ee80e07
Arsalan Ansari <arsalan8974@gmail.com>
2026-08-05 22:31:24 +0530

    Time: 0 ms (100%), Space: 18.4 MB (69.17%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool mirror(TreeNode* node1,TreeNode* node2){
+        if(node1==nullptr || node2==nullptr){
+            return node1==node2;
+        }
+        return (node1->val==node2->val) && (mirror(node1->left,node2->right)) && (mirror(node1->right,node2->left));
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root==nullptr) return true;;
+        return mirror(root->left,root->right);
+    }
+};
