a6a19451080316ad7edd6de8a472a7441b145a5b
Arsalan Ansari <arsalan8974@gmail.com>
2026-08-21 17:04:11 +0530

    Time: 0 ms (100%), Space: 10.7 MB (89.45%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,15 +11,37 @@
  */
 class Solution {
 public:
-    void makeinorder(TreeNode* root,vector<int> &inorder){
-        if(root==nullptr) return;
-        makeinorder(root->left,inorder);
-        inorder.push_back(root->val);
-        makeinorder(root->right,inorder);
-    }
+    // void makeinorder(TreeNode* root,vector<int> &inorder){
+    //     if(root==nullptr) return;
+    //     makeinorder(root->left,inorder);
+    //     inorder.push_back(root->val);
+    //     makeinorder(root->right,inorder);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>inorder;
-        makeinorder(root,inorder);
+        // makeinorder(root,inorder);
+        TreeNode* curr=root;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right!=nullptr && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=nullptr;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
         return inorder;
         // vector<int>inorder;
         // stack<TreeNode*>st;
