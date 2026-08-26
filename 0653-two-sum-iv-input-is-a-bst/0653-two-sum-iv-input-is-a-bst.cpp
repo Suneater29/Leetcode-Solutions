d00e44fa45ddbb4e6da5988e515e06dbc5f96325
Arsalan Ansari <arsalan8974@gmail.com>
2026-08-26 14:05:12 +0530

    Time: 0 ms (100%), Space: 37.2 MB (82.51%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,24 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTiterator{
+    stack<TreeNode*>st;
+    bool reverse=true;
+
+public:
+    BSTiterator(TreeNode* root,bool isreverse){
+        reverse=isreverse;
+        pushin(root);
+    }
+
+    bool hasNext(){
+        if(st.empty()) return false;
+        return true;
+    }
+
+    int next(){
+        TreeNode* topnode=st.top();
+        st.pop();
+        if(!reverse) pushin(topnode->right);
+        else pushin(topnode->left);
+        return topnode->val;
+    }
+
+private:
+    void pushin(TreeNode* root){
+        while(root!=nullptr){
+            st.push(root);
+            if(reverse==true) root=root->right;
+            else root=root->left;
+        }
+    }
+};
 class Solution {
 public:
-    void findin(TreeNode* root,vector<int> &inorder){
-        if(root==nullptr) return;
-        findin(root->left,inorder);
-        inorder.push_back(root->val);
-        findin(root->right,inorder);
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int>inorder;
-        findin(root,inorder);
-        int low=0;
-        int high=inorder.size()-1;
+        if(root==nullptr) return false;
+        BSTiterator left(root,false);
+        BSTiterator right(root,true);
+        int low=left.next();
+        int high=right.next();
         while(low<high){
-            int sum=inorder[low]+inorder[high];
-            if(sum>k) high--;
-            else if(sum<k) low++;
-            else return true;
+            if(low+high==k) return true;
+            else if(low+high<k) low=left.next();
+            else high=right.next();
         }
         return false;
     }
